6d2fa4bd9a2c5ccd0cfb2811a338a2ef9a212600
SlackingSlothh <zkyrbbn@gmail.com>
2026-04-15 14:45:40 +0700

    feat: add simple BFS algorithm

--- backend/src/algorithm/bfs.go
+++ backend/src/algorithm/bfs.go
@@ -1,2 +1,26 @@
 package algorithm
 
+import (
+	"github.com/luis/Tubes2_AyamCarbonara/backend/src/model"
+	"github.com/luis/Tubes2_AyamCarbonara/backend/src/selector"
+)
+
+func BFS(node *model.DOMNode, filter selector.Selector) []*model.DOMNode {
+	var queue []*model.DOMNode
+	queue = append(queue, node)
+
+	var result []*model.DOMNode
+	var current *model.DOMNode
+
+	for len(queue) > 0 {
+		current = queue[0]
+		queue = append(queue, current.Children...)
+		queue = queue[1:]
+
+		if (filter(current)) {
+			result = append(result, current)
+		}
+	}
+
+	return result
+}
