ee95bf6cb18e85153e32e7a5936cea73cfda2418
SlackingSlothh <zkyrbbn@gmail.com>
2026-04-15 14:29:14 +0700

    feat: add simple DFS algorithm

--- backend/src/algorithm/dfs.go
+++ backend/src/algorithm/dfs.go
@@ -1,2 +1,18 @@
 package algorithm
 
+import (
+	"github.com/luis/Tubes2_AyamCarbonara/backend/src/model"
+	"github.com/luis/Tubes2_AyamCarbonara/backend/src/selector"
+)
+
+func DFS(node *model.DOMNode, filter selector.Selector) []*model.DOMNode {
+	var result []*model.DOMNode
+	if (filter(node)) {
+		result = append(result, node)
+	}
+	for _, child := range node.Children {
+		childResult := DFS(child, filter)
+		result = append(result, childResult...)
+	}
+	return result
+}
