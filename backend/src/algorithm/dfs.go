7fd95f30e517612092ea14a2f26f5292c16266a6
SlackingSlothh <zkyrbbn@gmail.com>
2026-04-16 15:31:41 +0700

    refactor: change DFS to iterative

--- backend/src/algorithm/dfs.go
+++ backend/src/algorithm/dfs.go
@@ -6,13 +6,21 @@ import (
 )
 
 func DFS(node *model.DOMNode, filter selector.Selector) []*model.DOMNode {
-	var result []*model.DOMNode
-	if (filter(node)) {
-		result = append(result, node)
+	stack := []*model.DOMNode{node}
+	result := make([]*model.DOMNode, 0, 32)
+
+	for len(stack) > 0 {
+		current := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if filter(current) {
+			result = append(result, current)
+		}
+
+		for i := len(current.Children) - 1; i >= 0; i-- {
+			stack = append(stack, current.Children[i])
+		}
 	}
-	for _, child := range node.Children {
-		childResult := DFS(child, filter)
-		result = append(result, childResult...)
-	}
+
 	return result
 }
