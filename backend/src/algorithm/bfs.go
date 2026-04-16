2e2b56d62831ec036a145363a501d8cc32a59525
SlackingSlothh <zkyrbbn@gmail.com>
2026-04-16 08:36:35 +0700

    feat: parallel BFS

--- backend/src/algorithm/bfs.go
+++ backend/src/algorithm/bfs.go
@@ -1,6 +1,9 @@
 package algorithm
 
 import (
+	"runtime"
+	"sync"
+
 	"github.com/luis/Tubes2_AyamCarbonara/backend/src/model"
 	"github.com/luis/Tubes2_AyamCarbonara/backend/src/selector"
 )
@@ -23,4 +26,46 @@ func BFS(node *model.DOMNode, filter selector.Selector) []*model.DOMNode {
 	}
 
 	return result
+}
+
+func FastBFS(node *model.DOMNode, filter selector.Selector) []*model.DOMNode {
+	queue := make(chan *model.DOMNode, 100)
+    resultChan := make(chan *model.DOMNode, 100)
+
+    var wg sync.WaitGroup
+
+    worker := func() {
+        for n := range queue {
+            if filter(n) {
+                resultChan <- n
+            }
+
+            for _, child := range n.Children {
+                wg.Add(1)
+                queue <- child
+            }
+            wg.Done()
+        }
+    }
+
+    cpu := runtime.NumCPU()
+    for i := 0; i < cpu; i++ {
+        go worker()
+    }
+
+    wg.Add(1)
+    queue <- node
+
+    go func() {
+        wg.Wait()
+        close(queue)
+        close(resultChan)
+    }()
+
+    var result []*model.DOMNode
+    for r := range resultChan {
+        result = append(result, r)
+    }
+
+    return result
 }
