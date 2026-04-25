0de1420701ecb4ed922c35fe6c772ecced26fcbe
staplesmaster <nicholasluischandra@gmail.com>
2026-04-25 18:39:40 +0700

    fix : add lca old

--- backend/src/algorithm/lca.go
+++ backend/src/algorithm/lca.go
@@ -3,8 +3,32 @@ package algorithm
 import "github.com/luis/Tubes2_AyamCarbonara/backend/src/model"
 
 func FindLCA(root *model.DOMNode, id1, id2 int) *model.DOMNode {
-	lca, _ := FindLCAWithSteps(root, id1, id2)
-	return lca
+	search1 := FastBFS(root, func(node *model.DOMNode) bool {return node.Id == id1})
+	if (search1 == nil) {
+		return  nil
+	}
+	search2 := FastBFS(root, func(node *model.DOMNode) bool {return node.Id == id2})
+	if (search2 == nil) {
+		return  nil
+	}
+	return FindLCAByNode(search1[0], search2[0])
+}
+
+func FindLCAByNode(node1, node2 *model.DOMNode) *model.DOMNode {
+	for node1 != nil && node2 != nil {
+		if (node1.Depth > node2.Depth) {
+			node1 = node1.Parent
+		} else if node1.Depth < node2.Depth {
+			node2 = node2.Parent;
+		} else {
+			if node1 == node2 {
+				return  node1;
+			}
+			node1 = node1.Parent
+			node2 = node2.Parent
+		}
+	}
+	return nil;
 }
 
 func FindLCAWithSteps(root *model.DOMNode, id1, id2 int) (*model.DOMNode, []model.LCAStep) {
@@ -19,11 +43,6 @@ func FindLCAWithSteps(root *model.DOMNode, id1, id2 int) (*model.DOMNode, []mode
 	}
 
 	return FindLCAByNodeWithSteps(node1, node2)
-}
-
-func FindLCAByNode(node1, node2 *model.DOMNode) *model.DOMNode {
-	lca, _ := FindLCAByNodeWithSteps(node1, node2)
-	return lca
 }
 
 func FindLCAByNodeWithSteps(node1, node2 *model.DOMNode) (*model.DOMNode, []model.LCAStep) {
